b71325dc56d46698c58e3a5d87fe62d6354abc0d
Ardit Krasniqi <krasniqiardit01@gmail.com>
2017-10-25 23:59:56 +0200

    comments

--- js/URLParser.js
+++ js/URLParser.js
@@ -6,12 +6,12 @@
 */
 var URL = {
     path:document.location.href, // the current url to parse
-    get:function(key){ // this gets the value of the given key (example: '?name=myname') URL.get('name') returns "myname"
-        var val = this.path.substr(this.path.indexOf(key) + key.length + 1);
+    get:function(key){ // this returns the value of the given key (example: '?name=myname') URL.get('name') returns "myname"
+        var val = this.path.substr(this.path.indexOf(key) + key.length + 1); // substring path by the given key
         if(!this.path.includes(key)){ // checks if given argument exists on url
             throw new Error("Key \"" + key + "\" not found on URL"); // if not, throw Error
         }
-        // get the value
+        // if path includes '&', substring path from 0 to character '&'
         if(val.includes('&')){
             val = val.substring(0,val.indexOf('&'));
         }
